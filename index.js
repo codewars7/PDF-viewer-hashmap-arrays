fa0964982b0c1a4db6747fff0e845887beadf857
Americanman <snowboardracer7@gmail.com>
2020-04-28 03:28:20 -0400

    Replace Object with Map

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const alphabetStr = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
 const alphabet = alphabetStr.split(",")
 function designerPdfViewer(h, word) {
     // hash map
-    let myMap = Object.assign({}, null)
-    const hashMap = alphabet.map((item, index) => myMap[item] = h[index])
+    let myMap = new Map()
+    const hashMap = alphabet.map((item, index) => myMap.set(item, h[index]))
 
     // max lenght
-    let maxHeight = word.split('').reduce((acc,current) => acc < myMap[current] ? acc = myMap[current] : acc, 0)
+      let maxHeight = word.split('').reduce((acc,current) => acc < myMap.get(current) ? acc = myMap.get(current) : acc, 0)
 
     return maxHeight * word.length
 
